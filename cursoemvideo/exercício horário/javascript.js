864360fe56aa25d3ac4fd92eeeb9dd4a2595b5ce
Marcos-Moreira5112 <marcosoficialmoreira@gmail.com>
2025-06-26 23:36:00 -0400

    Adicionei troca de estilo conforme horário do dispositivo em JS

--- cursoemvideo/exercício horário/javascript.js
+++ cursoemvideo/exercício horário/javascript.js
@@ -1,9 +1,22 @@
 function carregar() {
 
-let msg = document.querySelector('#msg')
-let imagem = document.querySelector('#imagem')
-let data = new Date()
-let hora = data.getHours()
+    let msg = document.querySelector('#msg')
+    let imagem = document.querySelector('#imagem')
+    let data = new Date()
+    let hora = data.getHours()
 
-msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 23) {
+        imagem.src = 'imagens/imagem-manhã-redonda.png'
+        document.body.style.background = 'rgb(243, 223, 109)'
+
+    } else if (hora >= 12 && hora < 18) {
+        imagem.src = 'imagens/imagem-tarde-redonda.png'
+        document.body.style.background = 'rgb(243, 223, 109)'
+        
+    } else {
+        imagem.src = 'imagens/imagem-noite-redonda.png'
+        document.body.style.background = 'rgb(9, 4, 36)' 
+    }
 }
