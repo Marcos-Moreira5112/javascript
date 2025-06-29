43f3a4f01680cb40bc521c5ac0d525b606b5820e
Marcos-Moreira5112 <marcosoficialmoreira@gmail.com>
2025-06-29 19:05:27 -0400

    Organizei todos os arquivos para os exercícios de repetição

---
+++ cursoemvideo/aula13/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var cont = 1
+
+do {
+    console.log(`passo ${cont}`)
+    cont++
+} while (cont <= 6)
+ */
+console.log('Vai começar...')
+
+for (let cont = 1; cont <= 10; cont++) {
+    console.log(`passo ${cont}`)
+}
+
+console.log('Fim')
