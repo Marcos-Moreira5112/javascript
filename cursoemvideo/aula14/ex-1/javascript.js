806cd06b38960403b0df829f17408eb5489ef5f9
Marcos-Moreira5112 <Marcosoficialmoreira@gmail.com>
2025-07-02 17:14:08 -0400

    finalizei o exercício adicionando contagem regressiva, formatação adequada do resultado e mais clareza na estrutura do código

--- cursoemvideo/aula14/ex-1/javascript.js
+++ cursoemvideo/aula14/ex-1/javascript.js
@@ -8,16 +8,40 @@ function contar() {
    let V_fim = Number(fim.value)
    let V_passos = Number(passos.value)
    
+   if (V_passos <= 0) {
+      window.alert(`Passo inválido! Considerando passos = 1`)
+      V_passos = 1
+   }
+   
    if (inicio.value.length ==  0 || fim.value.length == 0 || passos.value.length == 0) {
       
       window.alert(`Preencha todos os campos antes de continuar`)
    } else {
       
-      res.innerHTML = ``
+      res.innerHTML = `resultado: <br>`
       
-      for (let cont = V_inicio; cont <= V_fim; cont += V_passos) {
-         
-         res.innerHTML += `${cont}, `
+         if (V_inicio < V_fim) {
+            
+            //Contagem Crescente
+            
+            for (let cont = V_inicio; cont <= V_fim; cont += V_passos) {
+               if (cont + V_passos > V_fim) {
+                  res.innerHTML += `${cont}`
+               } else {
+                  res.innerHTML += `${cont}, `
+               }
+            }
+         } else if (V_inicio > V_fim) { 
+            
+            //Contagem decrescente
+            
+            for (let cont = V_inicio; cont>= V_fim; cont -= V_passos){
+               if (cont - V_passos < V_fim) {
+                  res.innerHTML += `${cont}`
+               } else {
+                  res.innerHTML += `${cont}, `
+               }
+            }
+         }
       }
    }
-}
