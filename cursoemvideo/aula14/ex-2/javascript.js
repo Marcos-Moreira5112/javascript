221dca530e00f7c3ce07acac7978aaba46122146
Marcos-Moreira5112 <marcosoficialmoreira@gmail.com>
2025-07-12 21:39:57 -0400

    Terminei o último exercício de repetições

--- cursoemvideo/aula14/ex-2/javascript.js
+++ cursoemvideo/aula14/ex-2/javascript.js
@@ -2,5 +2,21 @@ let numero = document.querySelector("#numero")
 let tabuada = document.querySelector("#tabuada")
 
 function calcular() {
-   let V_numero = numero.value
+
+   if (numero.value.length == 0) {
+
+      alert("Por favor, digite um número!")
+
+   } else {
+
+      let V_numero = Number(numero.value)
+
+      tabuada.innerHTML = ""
+
+      for (c = 1; c <= 10; c++) {
+         let item = document.createElement("option")
+         item.text = `${V_numero} x ${c} = ${V_numero * c}`
+         tabuada.appendChild(item)
+      }
+   }
 }
